c54de9f3766d1a0c24fa9f22e8125a725daf7140
Mike Hanby <mhanby@uab.edu>
2014-08-22 18:29:22 -0500

    Added logic to the epel.pp class so that the epel repo can be enabled/disabled via repo_epel::enable_epel

--- manifests/epel.pp
+++ manifests/epel.pp
@@ -3,14 +3,19 @@
 # set of additional packages for Enterprise Linux, including, but
 # not limited to, Red Hat Enterprise Linux (RHEL),CentOS and
 # Scientific Linux (SL).
-class repo_epel::epel inherits repo_epel::params {
+class repo_epel::epel inherits repo_epel {
   
   #file { "/etc/yum.repos.d/epel${::operatingsystemmajrelease}.repo": ensure => absent, }
+  if $repo_epel::enable_epel {
+    $enabled = 1
+  } else {
+    $enabled = 0
+  }
     
   yumrepo { 'epel':
     baseurl  => "${repourl}/${urlbit}/${::architecture}",
     descr    => "Extra Packages for Enterprise Linux (EPEL) Repository - EL${::operatingsystemmajrelease} - ${::architecture}",
-    enabled  => '1',
+    enabled  => "${enabled}",
     gpgcheck => '1',
     gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${::operatingsystemmajrelease}",
     priority => '11',
